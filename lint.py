33dabc393e615c883f10c233e0ee3057e528af34
dslowik44 <dslowik500@gmail.com>
2024-09-01 18:18:09 -0400

    Dev (#1)
    * 2
    
    * 3

--- lint.py
+++ lint.py
@@ -1,11 +1,23 @@
 import sys 
 
+from io import StringIO
+
+from pylint.lint import Run
+from pylint.reporters.text import TextReporter
+
+pylint_output = StringIO()  # Custom open stream
+reporter = TextReporter(pylint_output)
+Run(["factorial.py"], reporter=reporter, exit=False)
+print(pylint_output.getvalue())  # Retrieve and print the text report
+sys.exit(1)
+
+"""
 from pylint import lint  
 
 THRESHOLD = 9  
 
-run = lint.Run(["factorial.py"]) #, do_exit=False) 
-
+run = lint.Run("factorial.py", exit=False) 
+print(run)
 score = run.linter.stats["global_note"]  
 
 if score < THRESHOLD: 
@@ -16,3 +28,4 @@ if score < THRESHOLD:
 
 
 sys.exit(0) 
+"""
